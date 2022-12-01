f2bb4d419fb419e2d1a2a2769490b654a52db28f
VladBorod <jazgigantsmith@gmail.com>
2022-12-01 16:02:53 +0300

    Ex1 final edition

---
+++ Ex1Obj41ZeroSearch/Ex1Obj41ZeroSearch.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.WriteLine("Введите количество чисел: ");
+int amountNumbers = Convert.ToInt32(Console.ReadLine());
+int[] arr = UserNumbersInput(amountNumbers);
+Console.WriteLine($"Число элементов больше нуля -> {AboveZeroCount(arr)}");
+
+int[] UserNumbersInput(int num)
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите число {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+int AboveZeroCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+
